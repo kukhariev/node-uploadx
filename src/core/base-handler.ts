e87280a4748cecf334e60ef1abdbf6acf6d7e65a
kukhariev <q_h@inbox.ru>
2019-07-09 02:04:46 +0300

    fix: allow external cors middleware

--- src/core/base-handler.ts
+++ src/core/base-handler.ts
@@ -2,6 +2,9 @@ import * as http from 'http';
 import { File } from './interfaces';
 
 export abstract class BaseHandler {
+  allowedMethods = 'GET,PUT,PATCH,POST,DELETE';
+  allowedHeaders = '';
+  corsMaxAge = 600;
   /**
    * Create file
    */
@@ -24,20 +27,24 @@ export abstract class BaseHandler {
    * @internal
    */
   setOrigin(req: http.IncomingMessage, res: http.ServerResponse) {
-    req.headers.origin && res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    req.headers.origin && this.setHeader(res, 'Access-Control-Allow-Origin', req.headers.origin);
   }
 
   /**
    * OPTIONS preflight Request
-   * @internal
    */
   preFlight(req: http.IncomingMessage, res: http.ServerResponse) {
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,HEAD');
-    res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers']!);
+    this.setHeader(res, 'Access-Control-Allow-Methods', this.allowedMethods);
+    const allowedHeaders = this.allowedHeaders || req.headers['access-control-request-headers']!;
+    this.setHeader(res, 'Access-Control-Allow-Headers', allowedHeaders);
+    this.setHeader(res, 'Access-Control-Max-Age', this.corsMaxAge);
     res.writeHead(204);
     res.end();
   }
 
+  private setHeader(res: http.ServerResponse, name: string, value: string | string[] | number) {
+    !res.hasHeader(name) && res.setHeader(name, value);
+  }
   /**
    * Make response
    */
