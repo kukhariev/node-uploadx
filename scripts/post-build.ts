52fdba29ec4f63f28af150261bac932f501fddd7
kukhariev <q_h@inbox.ru>
2019-07-19 12:11:14 +0300

    build:  refactor

--- scripts/post-build.ts
+++ scripts/post-build.ts
@@ -1,20 +1,25 @@
 import * as fs from 'fs';
-import * as util from 'util';
-const copyFile = util.promisify(fs.copyFile);
-const DIST = 'dist';
 
-(async () => {
-  try {
-    const pkg = JSON.parse(fs.readFileSync('package.json').toString());
-    delete pkg['devDependencies'];
-    delete pkg['scripts'];
-    delete pkg['husky'];
-    delete pkg['lint-staged'];
-    fs.writeFileSync(`${DIST}/package.json`, JSON.stringify(pkg, undefined, 2));
-    await copyFile('LICENSE', `${DIST}/LICENSE`);
-    await copyFile('README.md', `${DIST}/README.md`);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-})();
+try {
+  const oldPackage = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' }));
+  const newPackage: any = {};
+  newPackage.name = oldPackage.name;
+  newPackage.version = oldPackage.version;
+  newPackage.description = oldPackage.description;
+  newPackage.keywords = oldPackage.keywords;
+  newPackage.author = oldPackage.author;
+  newPackage.repository = oldPackage.repository;
+  newPackage.homepage = oldPackage.homepage;
+  newPackage.license = oldPackage.license;
+  newPackage.files = ['lib'];
+  newPackage.typings = 'types/index.d.ts';
+  newPackage.main = 'lib/index.js';
+  newPackage.dependencies = oldPackage.dependencies;
+  newPackage.engines = oldPackage.engines;
+  fs.writeFileSync(`dist/package.json`, JSON.stringify(newPackage, undefined, 2));
+  fs.copyFileSync('LICENSE', `dist/LICENSE`);
+  fs.copyFileSync('README.md', `dist/README.md`);
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
