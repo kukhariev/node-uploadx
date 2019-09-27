f086855ff4297b8508f1fe8e53132422db28b5c0
kukhariev <q_h@inbox.ru>
2019-09-28 00:00:27 +0300

    chore: cors tweaks

--- src/core/Cors.ts
+++ src/core/Cors.ts
@@ -1,17 +1,21 @@
 import * as http from 'http';
+import { getHeader } from './utils';
+
 export class Cors {
   static allowedMethods = ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT'];
+  static allowedHeaders = [];
   static maxAge = 600;
-  static origin = ['*'];
+  static origin = '*';
 
   static preflight(req: http.IncomingMessage, res: http.ServerResponse): boolean {
-    const origin = req.headers.origin as string;
+    const origin = getHeader(req, 'origin');
     if (!origin) return false;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    const isPreflight = req.headers['access-control-request-method'] && req.method === 'OPTIONS';
+    res.setHeader('Access-Control-Allow-Origin', Cors.origin || origin);
+    const isPreflight = getHeader(req, 'access-control-request-method') && req.method === 'OPTIONS';
     if (!isPreflight) return false;
     res.setHeader('Access-Control-Allow-Methods', Cors.allowedMethods.toString());
-    const allowedHeaders = req.headers['access-control-request-headers'] as string;
+    const allowedHeaders =
+      Cors.allowedHeaders.toString() || getHeader(req, 'access-control-request-headers');
     allowedHeaders && res.setHeader('Access-Control-Allow-Headers', allowedHeaders);
     res.setHeader('Access-Control-Max-Age', Cors.maxAge);
     return true;
