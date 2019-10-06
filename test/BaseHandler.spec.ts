73899f25c1e990ddf22607e76cd4998a2f670276
kukhariev <q_h@inbox.ru>
2019-10-06 23:05:18 +0300

    refactor:  move getFileId(), get() to super class

--- test/BaseHandler.spec.ts
+++ test/BaseHandler.spec.ts
@@ -1,12 +1,39 @@
 import { expect } from 'chai';
+import { createRequest } from 'node-mocks-http';
 import { BaseHandler, BaseStorage } from '../src';
 class TestUploader extends BaseHandler {
   storage = (null as unknown) as BaseStorage;
 }
 describe('BaseHandler', function() {
-  let uploader: TestUploader;
   it('should create instance BaseHandler', function() {
-    uploader = new TestUploader();
-    expect(uploader).to.be.instanceOf(BaseHandler);
+    expect(new TestUploader()).to.be.instanceOf(BaseHandler);
+  });
+  describe.only('getFileId', function() {
+    const valid = [
+      '/981e0d3bb9f93bfa62ef7938ff001308/981e0d3bb9f93bfa62ef7938ff001308',
+      '/981e0d3bb9f93bfa62ef7938ff001308?upload_id=981e0d3bb9f93bfa62ef7938ff001308'
+    ];
+    const invalid = [
+      '/',
+      '/981e0d3bb9f93bfa62ef7938ff001308',
+      '/981e0d3bb9f93bfa62ef7938ff001308?bad_id=981e0d3bb9f93bfa62ef7938ff001308'
+    ];
+    let uploader: any;
+    beforeEach(function() {
+      uploader = new TestUploader();
+    });
+
+    it('should return id', function() {
+      valid.forEach(url => {
+        expect(uploader.getFileId(createRequest({ url }))).to.be.equal(
+          '981e0d3bb9f93bfa62ef7938ff001308'
+        );
+      });
+    });
+    it('should return undefined', function() {
+      invalid.forEach(url => {
+        expect(uploader.getFileId(createRequest({ url }))).to.be.undefined;
+      });
+    });
   });
 });
