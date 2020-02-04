b604c1e5d0ab11c1781193ae421862498c4203e8
kukhariev <q_h@inbox.ru>
2020-02-04 22:41:46 +0300

    test: fix storageOptions type

--- test/fixtures/index.ts
+++ test/fixtures/index.ts
@@ -26,7 +26,7 @@ export const testfile = {
 export const metafile = testfile.name + METAFILE_EXTNAME;
 export const filename = testfile.name;
 
-export const storageOptions: BaseStorageOptions = {
+export const storageOptions: BaseStorageOptions<File> = {
   filename: file => `${file.userId}/${file.originalName}`,
   maxUploadSize: '6GB',
   allowMIME: ['video/*', 'image/*', 'application/octet-stream'],
