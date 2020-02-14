d00e05bb338851d00b6b718507d19e4e3b6716db
q_h <q_h@inbox.ru>
2020-02-14 12:05:23 +0300

    chore: fix tus example

--- examples/express-tus.ts
+++ examples/express-tus.ts
@@ -1,18 +1,21 @@
 import * as express from 'express';
 import { promises } from 'fs';
+import { join } from 'path';
 import { DiskStorageOptions, tus } from '../src';
 
 const app = express();
-
+const dir = 'files';
 const opts: DiskStorageOptions = {
+  allowMIME: ['image/*'],
+  directory: dir,
   filename: file => `.${file.originalName}`, // dot hide incomplete uploads
   onComplete: async file => {
     console.log('File upload complete: ', file.originalName);
-    await promises.rename(file.name, file.originalName); // unhide
+    await promises.rename(join(dir, file.name), join(dir, file.originalName)); // unhide
   }
 };
 
-app.use('/files', [express.static('files'), tus(opts)]);
+app.use('/files', [express.static(dir), tus(opts)]);
 
 app.listen(3003, error => {
   if (error) throw error;
