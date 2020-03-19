edb6fa0454bb4d068268569a92bd559fe97cffa2
q_h <q_h@inbox.ru>
2020-03-19 12:00:40 +0300

    build: move optionalDependencies to build script

--- scripts/post-build.ts
+++ scripts/post-build.ts
@@ -1,5 +1,10 @@
 import * as fs from 'fs';
-
+const optionalDependencies = {
+  'abort-controller': '^3.0.0',
+  'aws-sdk': '^2.616.0',
+  'google-auth-library': '^5.9.2',
+  'node-fetch': '^2.6.0'
+};
 try {
   const rootPackage = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' }));
   const libPackage: any = {};
@@ -14,7 +19,7 @@ try {
   libPackage.typings = 'types/index.d.ts';
   libPackage.main = 'index.js';
   libPackage.dependencies = rootPackage.dependencies;
-  libPackage.optionalDependencies = rootPackage.optionalDependencies;
+  libPackage.optionalDependencies = optionalDependencies;
   libPackage.engines = rootPackage.engines;
   fs.writeFileSync(`dist/package.json`, JSON.stringify(libPackage, undefined, 2));
   fs.copyFileSync('LICENSE', `dist/LICENSE`);
