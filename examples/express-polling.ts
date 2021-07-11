b8c8c0e0b2066dd7992f24d197045217dedc824f
kukhariev <q_h@inbox.ru>
2021-07-11 23:02:43 +0300

    chore: add post-upload polling example

---
+++ examples/express-polling.ts
@@ -0,0 +1,54 @@
+import { DiskFile, DiskStorage, Uploadx } from '@uploadx/core';
+import * as copyFile from 'cp-file';
+import * as express from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const app = express();
+
+type Moving = { percent: number; status: 'moving' | 'error' | 'done' };
+
+const processes = {} as Record<string, Moving>;
+
+const uploadDirectory = 'upload';
+const moveTo = 'upload';
+
+const storage = new DiskStorage({ directory: uploadDirectory });
+
+const onComplete: express.RequestHandler = (req, res) => {
+  const file = req.body as DiskFile;
+  const moving = (processes[file.name] ??= {} as Moving);
+  if (!moving.status) {
+    moving.status = 'moving';
+    const source = path.resolve(uploadDirectory, file.name);
+    const destination = path.resolve(moveTo, file.originalName);
+    void (async () => {
+      try {
+        await copyFile(source, destination).on('progress', ({ percent }) => {
+          moving.percent = percent * 100;
+        });
+        await fs.promises.unlink(source);
+        moving.status = 'done';
+      } catch (e) {
+        console.error(e);
+        moving.status = 'error';
+      }
+    })();
+  }
+  if (moving.status === 'error') {
+    res.status(422).json({ ...file, moving });
+  } else if (moving.status === 'done') {
+    res.json({ ...file, moving });
+  } else {
+    res
+      .status(202)
+      // .set('Retry-After', '5') // override polling interval
+      .json({ ...file, moving });
+  }
+};
+
+const uploadx = new Uploadx({ storage });
+
+app.use('/files', uploadx.upload, onComplete);
+
+app.listen(3002, () => console.log('listening on port:', 3002));
