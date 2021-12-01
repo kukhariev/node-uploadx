37050470e187c59934501271d8175b71e2b9ae8f
kukhariev <q_h@inbox.ru>
2021-12-01 20:42:42 +0300

    test: add no expiry cache test

--- test/cache.spec.ts
+++ test/cache.spec.ts
@@ -1,25 +1,37 @@
 import { Cache } from '../packages/core/src';
 
 describe('Cache', () => {
-  let cache: Cache<any>;
+  let cache: Cache<number>;
 
-  beforeEach(() => {
-    cache = new Cache(5, 60);
+  const fill = (): void => {
     for (let i = 0; i < 10; i++) {
       cache.set(`key-${i}`, i);
     }
-  });
+  };
 
   it('should limit size', () => {
+    cache = new Cache(5, 60);
+    fill();
     expect(cache.get(`key-0`)).toBeUndefined();
     expect(cache.get(`key-9`)).toBe(9);
     expect(cache['_map'].size).toBe(5);
   });
 
   it('should expire', async () => {
+    cache = new Cache(5, 60);
+    fill();
     jest.useFakeTimers();
     jest.advanceTimersByTime(100_000);
     expect(cache.get(`key-9`)).toBeUndefined();
     expect(cache['_map'].size).toBe(4);
+  });
+
+  it('should not expire if maxAge is 0', async () => {
+    cache = new Cache(5, 0);
+    fill();
+    jest.useFakeTimers();
+    jest.advanceTimersByTime(100_000);
+    expect(cache.get(`key-9`)).toBe(9);
+    expect(cache['_map'].size).toBe(5);
   });
 });
