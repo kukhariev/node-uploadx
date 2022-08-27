1fd81633c2a224f22e5a510134da10bf81a02219
kukhariev <q_h@inbox.ru>
2022-08-27 13:48:27 +0300

    test: update snapshots

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ const { pathsToModuleNameMapper } = require('ts-jest');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  snapshotFormat: {
+    'escapeString': false,
+    'printBasicPrototype': false
+  },
   testRegex: '.*\\.(spec|test)\\.ts$',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   modulePathIgnorePatterns: ['<rootDir>/package.json'],
