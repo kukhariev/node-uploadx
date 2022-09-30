3dc010425488a96fe7f77e2fafb90f46e5061ddf
kukhariev <q_h@inbox.ru>
2022-09-30 22:43:19 +0300

    chore: update fastify example

--- examples/fastify.ts
+++ examples/fastify.ts
@@ -1,28 +1,25 @@
-import fastify from 'fastify';
-import middie from 'middie';
+import Fastify from 'fastify';
+import middie from '@fastify/middie';
 import { Uploadx } from '@uploadx/core';
 import { join } from 'path';
 import pino from 'pino';
 
 const logger = pino({ level: process.env.LOG_LEVEL || 'info', name: 'uploadx' });
 
-const PORT = process.env.PORT || 3002;
+const port = process.env.PORT || 3002;
 
-const server = fastify({ logger: true });
-const uploadx = new Uploadx({ directory: 'upload', logger });
-uploadx.on('completed', ({ name, originalName }) =>
-  server.log.info(
-    `upload complete, path: ${join('files', name)}, original filename: ${originalName}`
-  )
-);
+async function build() {
+  const uploadx = new Uploadx({ directory: 'upload', logger });
+  uploadx.on('completed', ({ name, originalName }) =>
+    logger.info(`upload complete, path: ${join('files', name)}, original filename: ${originalName}`)
+  );
+  const fastify = Fastify({ logger });
+  await fastify.register(middie);
 
-server.register(middie).then(
-  () => {
-    void server.use('/files', uploadx.handle);
-    return server.listen(PORT, (err, address) => {
-      if (err) throw err;
-      server.log.info(`server listening on ${address}`);
-    });
-  },
-  e => console.error(e)
-);
+  void fastify.use(uploadx.handle);
+  return fastify;
+}
+
+build()
+  .then(fastify => fastify.listen({ port: +port }))
+  .catch(logger.error);
