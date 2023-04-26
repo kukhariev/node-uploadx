2ee9560b7586256c7d5cafa3af8b7b464746222f
q_h <q_h@inbox.ru>
2023-04-26 12:52:42 +0300

    chore: update GCS example

--- examples/gcs-direct.ts
+++ examples/gcs-direct.ts
@@ -1,9 +1,11 @@
-import { MetaStorage, Uploadx } from '@uploadx/core';
+import { cors, MetaStorage, Uploadx } from '@uploadx/core';
 import { GCStorage } from '@uploadx/gcs';
 import { createServer } from 'http';
 import { parse } from 'url';
 
 const PORT = process.env.PORT || 3002;
+
+const corsHandler = cors();
 
 // Don't forget to set GCS_BUCKET and GCS_KEYFILE environment variables
 const storage = new GCStorage({
@@ -14,17 +16,16 @@ const storage = new GCStorage({
   metaStorage: new MetaStorage()
 });
 
-const uploads = new Uploadx({ storage });
-uploads.on('created', file =>
+const uploadx = new Uploadx({ storage });
+uploadx.on('created', file =>
   console.log('google upload link sent to client: ', file.GCSUploadURI)
 );
 
 createServer((req, res) => {
   const { pathname } = parse(req.url || '');
   if (pathname === '/files') {
-    uploads.handle(req, res);
+    uploadx.handle(req, res);
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plan' });
-    res.end('Not Found');
+    corsHandler(req, res, () => uploadx.send(res, { body: 'Not Found', statusCode: 404 }));
   }
 }).listen(PORT, () => console.log('listening on port:', PORT));
