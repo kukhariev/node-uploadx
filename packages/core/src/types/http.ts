0305f480798e701ee6a8623328659c922afc4fc1
q_h <q_h@inbox.ru>
2026-05-29 13:16:54 +0300

    refactor(core): add generic type params and clean up response types

--- packages/core/src/types/http.ts
+++ packages/core/src/types/http.ts
@@ -6,19 +6,18 @@ export type IncomingMessage = http.IncomingMessage & {
 
 export type ServerResponse = http.ServerResponse;
 
-export interface IncomingMessageWithBody<T = any> extends http.IncomingMessage {
+export interface IncomingMessageWithBody<T = unknown> extends http.IncomingMessage {
   body?: T;
   _body?: boolean;
 }
 
 export type Headers = Record<string, number | string | string[]>;
 
-export type ResponseBody = string | Record<string, any>;
 export type ResponseBodyType = 'text' | 'json';
 
-export type ResponseTuple<T = ResponseBody> = [statusCode: number, body?: T, headers?: Headers];
+export type ResponseTuple<T = unknown> = [statusCode: number, body?: T, headers?: Headers];
 
-export interface UploadxResponse<T = ResponseBody> {
+export interface UploadxResponse<T = unknown> {
   statusCode?: number;
   headers?: Headers;
   body?: T;
