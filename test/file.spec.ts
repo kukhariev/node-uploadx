df031454de9c6681cdc12c44b2ef7bcf274c527f
q_h <q_h@inbox.ru>
2026-07-03 10:45:51 +0300

    fix(core): improve filename validation

--- test/file.spec.ts
+++ test/file.spec.ts
@@ -4,15 +4,31 @@ describe('File', () => {
   describe('FileName', () => {
     it.each([
       ['', false],
+      ['.', false],
       ['..', false],
       ['c:\\abs', false],
-      ['12', false],
+      ['1', true],
       ['filename?.ext', false],
       ['../filename.ext', false],
       ['/filename.ext', false],
-      ['filename.ext', true]
+      ['filename.ext', true],
+      ['.filename', true],
+      ['file..ext', true],
+      ['foo/bar', true]
     ])('isValid(%s) === %s', (str, expected) => {
       expect(FileName.isValid(str)).toBe(expected);
+    });
+
+    it.each([
+      ['null byte', '\0'],
+      ['newline', '\n'],
+      ['tab', '\t'],
+      ['carriage return', '\r'],
+      ['DEL', '\x7F'],
+      ['safe\\n.txt', 'safe\n.txt'],
+      ['safe\\x7F.txt', 'safe\x7F.txt']
+    ])('rejects %s', (_, input) => {
+      expect(FileName.isValid(input)).toBe(false);
     });
   });
 });
